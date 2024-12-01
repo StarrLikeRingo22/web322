23b1f5c0f5c922496cc39553c33e80fa89508910
Abdalla Abdelgadir <Abdallaabdelgaddir@gmail.com>
2024-11-30 22:02:00 -0500

    added views

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ const port = process.env.PORT || 8080
 
 
 app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, "public")))
 app.use(express.urlencoded({ extended: true })) // Add this line for form data parsing
 
